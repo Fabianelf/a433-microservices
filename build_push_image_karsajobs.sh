53b4509058fe9b3f385f74febbc10cdd0357d457
Fabian Elfri Syahputra <mymail2507@gmail.com>
2023-10-14 19:24:15 +0700

    Build Push Image Karsajob.sh

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,7 @@
+#membuat Docker image dari Dockerfile yang tadi dibuat, dengan nama image item-app, dan memiliki tag v1.
+docker build -t elffree/karsajobs:latest .
+#Login ke Docker Hub via Terminal.
+docker login
+echo $PASSWORD_DOCKER_HUB | docker login -u elffree --password-stdin
+#Mengunggah image ke Docker Hub
+docker push elffree/karsajobs:latest
